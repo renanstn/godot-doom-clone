cff431078c4b879b4c0436d923013cc068ef9e66
Renan Santana <renan_ota@hotmail.com>
2020-04-02 18:42:36 -0300

    Upload inicial

---
+++ doom-clone/Scripts/Player.gd
@@ -0,0 +1,48 @@
+extends KinematicBody
+
+
+const MOVE_SPEED = 4
+const MOUSE_SENS = 0.5
+
+onready var anim_player = $AnimationPlayer
+onready var raycast = $RayCast
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	yield(get_tree(), "idle_frame")
+	get_tree().call_group("zombies", "set_player", self)
+	# No comando acima, estamos chamando a função set_player, de todos que
+	# estão no grupo 'zombies', passando como parâmetro o self
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= MOUSE_SENS * event.relative.x
+		
+func _process(delta):
+	if Input.is_action_pressed("exit"):
+		get_tree().quit()
+	if Input.is_action_pressed("restart"):
+		kill()
+
+func _physics_process(delta):
+	var move_vector:Vector3 = Vector3()
+	if Input.is_action_pressed("move_forwards"):
+		move_vector.z -= 1
+	if Input.is_action_pressed("move_backwards"):
+		move_vector.z += 1
+	if Input.is_action_pressed("move_left"):
+		move_vector.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_vector.x += 1
+	move_vector = move_vector.normalized()
+	move_vector = move_vector.rotated(Vector3(0, 1, 0), rotation.y)
+	move_and_collide(move_vector * MOVE_SPEED * delta)
+		
+	if Input.is_action_pressed("shoot") and not anim_player.is_playing():
+		anim_player.play("shoot")
+		var coll = raycast.get_collider()
+		if raycast.is_colliding() and coll.has_method("kill"):
+			coll.kill()
+		
+func kill():
+	get_tree().reload_current_scene()
---
+++ doom-clone/Scripts/Zombie.gd
@@ -0,0 +1,39 @@
+extends KinematicBody
+
+
+const MOVE_SPEED = 3
+
+onready var raycast = $RayCast
+onready var anim_player = $AnimationPlayer
+
+var player = null
+var dead = false
+
+func _ready():
+	anim_player.play("walk")
+	add_to_group("zombies")
+
+func _physics_process(delta):
+	if dead:
+		return
+	if player == null:
+		return
+
+	var vector_to_player = player.translation - translation
+	vector_to_player = vector_to_player.normalized()
+	raycast.cast_to = vector_to_player * 1.5
+
+	move_and_collide(vector_to_player * MOVE_SPEED * delta)
+
+	if raycast.is_colliding():
+		var coll = raycast.get_collider()
+		if coll != null and coll.name == 'Player':
+			coll.kill()
+
+func set_player(p):
+	player = p
+
+func kill():
+	dead = true
+	$CollisionShape.disabled = true
+	anim_player.play("die")
